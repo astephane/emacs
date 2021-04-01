0403e1b2c417571840f0b744c446b15d463f3371
Stéphane ALBERT <stephane.albert@geown.io>
2021-04-01 11:14:00 +0200

    Added BAT script to run emacs in the Visual Studio development environment.

---
+++ runemacs.bat
@@ -0,0 +1,5 @@
+@echo off
+REM call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
+call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\VC\Auxiliary\Build\vcvars64.bat"
+REM C:\Users\FRAGO_1\emacs-26.3-x86_64\bin\runemacs.exe
+%USERPROFILE%\emacs-26.3-x86_64\bin\runemacs.exe
